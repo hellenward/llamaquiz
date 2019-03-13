ec52bc806fa01a7e2df97780d7454284fde295e9
Hellen Ward <hellenwellsward@gmail.com>
2019-03-13 07:47:30 +0000

    Added in score keeping and final score

--- quiz.sh
+++ quiz.sh
@@ -51,6 +51,10 @@ then
   echo
   echo "You're absolutely correct!  It's pronounced KREE-uh.  Llamas usually only have one baby at a time (twins are rare) and gestation is 350 days - almost a whole year!"
   echo
+  score=$((score+1))
+  echo "Your score is" $score
+  echo
+  echo
 else
   echo
   echo
@@ -64,6 +68,8 @@ else
   echo "Afraid not!  The correct answer is b - Cria. It's pronounced KREE-uh.  Llamas usually only have one baby at a time (twins are rare) and gestation is 350 days - almost a whole year!"
   echo
   echo "Your score is" $score
+  echo
+  echo
 fi
 
 echo "Question 2: Where do llamas originate from? (a - Peru, b - Iceland, c - The Moon, duh!)"
@@ -81,6 +87,11 @@ then
   echo "    '' ''   '' ''"
   echo
   echo "You genius, you! Llamas definitely come from Peru!"
+  score=$((score+1))
+  echo
+  echo "Your score is" $score
+  echo
+  echo
 else
   echo
   echo
@@ -92,6 +103,10 @@ else
   echo "    |||   |||"
   echo
   echo "No way, llamas enjoy somewhere warm with plenty of oxygen.  Peru is perfect for your average llama!"
+  echo
+  echo "Your score is" $score
+  echo
+  echo
 fi
 
 echo "Question 3.  What would a llama do if you gave them too much to carry? (a - they would lie down and refuse to move, b - they would bite you or c - they'd tell you to carry your own stuff, lazy human!"
@@ -108,6 +123,11 @@ then
   echo "    zz zz"
   echo
   echo "Yep, llamas are pretty smart and know their own limits.  If you overload them, they will just stay still till you realise your mistake!"
+  score=$((score+1))
+  echo
+  echo "Your score is" $score
+  echo
+  echo
 else
   echo
   echo
@@ -117,6 +137,28 @@ else
   echo "  '|||||||||_'___o"
   echo
   echo "No, silly!  Llamas don't bite or speak any human language.  They know their own limits though (clever things), and will just stay still till you realise your mistake!"
+  echo
+  echo "Your score is" $score
+  echo
+  echo
 fi
 
+if [ $score == 0 ]
+then
+  echo "Your final score is "$score
+  echo "Better luck next time!"
+  echo
+  echo
+elif [ $score -le 2 ]
+then
+  echo "Your final score is "$score
+  echo "You're well on your way to becoming a llama expert!"
+  echo
+  echo
+else
+  echo "Your final score is "$score
+  echo "You are the llama champion!"
+  echo
+  echo
+fi
 echo "Thanks for taking the llama quiz!"
