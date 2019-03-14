59c98b5256e77e7f8bdfe136084a441a4186978a
Hellen Ward <hellenwellsward@gmail.com>
2019-03-14 06:53:54 +0000

    added in a dancing llama at the end

--- quiz.sh
+++ quiz.sh
@@ -161,4 +161,30 @@ else
   echo
   echo
 fi
-echo "Thanks for taking the llama quiz!"
+echo "Press any key to continue"
+read continue
+
+while true ; do
+
+clear ; cat << EOT
+    \/
+  <'l
+    ll
+    llama~
+    || ||
+    '' ''
+EOT
+
+sleep 0.1
+
+clear ; cat << EOT
+  //
+<-l
+  ll
+  llama~
+  zz zz
+EOT
+
+sleep 0.2
+
+done
